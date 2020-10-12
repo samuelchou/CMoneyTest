a8743d76a12c7d50b1884198e4d7732853bb6221
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 14:34:51 +0800

    <Add> 頁面一：按鈕可以切換到頁面二。

--- app/src/main/java/com/cmoney/testlab/view/ActivityLauncher.java
+++ app/src/main/java/com/cmoney/testlab/view/ActivityLauncher.java
@@ -1,6 +1,8 @@
 package com.cmoney.testlab.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,12 @@ public class ActivityLauncher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        findViewById(R.id.btnRequest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityLauncher.this, ActivityList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
