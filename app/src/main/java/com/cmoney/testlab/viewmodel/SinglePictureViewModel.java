bc736766fe0656a4063fd3c5df1d0a8a32dbed4f
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 16:11:14 +0800

    <Behavior> 頁面三：引入Loading畫面，優化使用者體驗。

--- app/src/main/java/com/cmoney/testlab/viewmodel/SinglePictureViewModel.java
+++ app/src/main/java/com/cmoney/testlab/viewmodel/SinglePictureViewModel.java
@@ -1,5 +1,6 @@
 package com.cmoney.testlab.viewmodel;
 
+import androidx.databinding.ObservableBoolean;
 import androidx.databinding.ObservableField;
 
 import com.cmoney.testlab.model.PictureDownloader;
@@ -8,6 +9,7 @@ import com.cmoney.testlab.model.SinglePicture;
 public class SinglePictureViewModel {
     private PictureDownloader pictureDownloader;
 
+    public final ObservableBoolean isLoading = new ObservableBoolean(false);
     public final ObservableField<SinglePicture> picture = new ObservableField<>();
 
     public SinglePictureViewModel() {
@@ -15,9 +17,11 @@ public class SinglePictureViewModel {
     }
 
     public void Request(int id) {
+        isLoading.set(true);
         pictureDownloader.RequestSinglePicture(id, new PictureDownloader.OnFinishCallback() {
             @Override
             public void onSuccess(SinglePicture result) {
+                isLoading.set(false);
                 picture.set(result);
             }
 
