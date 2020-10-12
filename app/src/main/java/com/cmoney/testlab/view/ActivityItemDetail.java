ad0ebbc36de5bd3e530e3d212bf109cfd2eb1591
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:28:54 +0800

    <Add> 頁面三：引入Data Binding並實作View Model. 現在會依據Model回傳資料了。

--- app/src/main/java/com/cmoney/testlab/view/ActivityItemDetail.java
+++ app/src/main/java/com/cmoney/testlab/view/ActivityItemDetail.java
@@ -2,20 +2,27 @@ package com.cmoney.testlab.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.cmoney.testlab.R;
+import com.cmoney.testlab.viewmodel.SinglePictureViewModel;
 
 public class ActivityItemDetail extends AppCompatActivity {
     public static final String ARG_ID = "Arg_ID";
 
+    private SinglePictureViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
+        viewModel = new SinglePictureViewModel();
+
+        com.cmoney.testlab.databinding.ActivityItemDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_item_detail);
+        binding.setViewModel(viewModel);
+
         Intent intent = getIntent();
         if (intent.getExtras() == null) {
             Toast.makeText(this, R.string.msg_fail_to_load, Toast.LENGTH_SHORT).show();
@@ -29,12 +36,10 @@ public class ActivityItemDetail extends AppCompatActivity {
             return;
         }
 
-        SetupContent(id);
+        FetchContent(id);
     }
 
-    private void SetupContent(int id) {
-        TextView itemTitle = findViewById(R.id.itemTitle);
-        itemTitle.setText(id + "");
-
+    private void FetchContent(int id) {
+        viewModel.Request(id);
     }
 }
