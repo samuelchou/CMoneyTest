950a6596a986a68c20d5eb5bcf99955ab36b2530
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:37:35 +0800

    <Add> View Model: 新增GalleryViewModel.

---
+++ app/src/main/java/com/cmoney/testlab/viewmodel/GalleryViewModel.java
@@ -0,0 +1,33 @@
+package com.cmoney.testlab.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.cmoney.testlab.model.PictureDownloader;
+import com.cmoney.testlab.model.SinglePicture;
+
+import java.util.List;
+
+public class GalleryViewModel {
+    private final MutableLiveData<List<SinglePicture>> pictureList = new MutableLiveData<>();
+
+    private PictureDownloader pictureDownloader;
+
+    public GalleryViewModel() {
+        pictureDownloader = new PictureDownloader();
+    }
+
+    public void FetchGallery() {
+        pictureDownloader.RequestPictureGallery(new PictureDownloader.OnGalleryFinishCallback() {
+            @Override
+            public void onSuccess(List<SinglePicture> result) {
+                pictureList.setValue(result);
+            }
+
+            @Override
+            public void onFail(String msg) {
+
+            }
+        });
+    }
+
+}
