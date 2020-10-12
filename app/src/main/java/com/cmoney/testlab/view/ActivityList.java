c3b9551775f71958c8a8a96e1c6e3e731d66f79e
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 14:34:32 +0800

    <Add> 新增頁面二：ActivityList。

---
+++ app/src/main/java/com/cmoney/testlab/view/ActivityList.java
@@ -0,0 +1,16 @@
+package com.cmoney.testlab.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cmoney.testlab.R;
+
+public class ActivityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
