0d815ea30977729a84654718a131e7c3920010ee
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:54:30 +0800

    <Add> 頁面三：實作Data Binding，現在會透過View Model更新Gallery了。

--- app/src/main/java/com/cmoney/testlab/view/ActivityList.java
+++ app/src/main/java/com/cmoney/testlab/view/ActivityList.java
@@ -3,22 +3,35 @@ package com.cmoney.testlab.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmoney.testlab.R;
+import com.cmoney.testlab.databinding.ActivityListBinding;
+import com.cmoney.testlab.model.SinglePicture;
+import com.cmoney.testlab.viewmodel.GalleryViewModel;
 import com.cmoney.testlab.viewmodel.PictureListAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityList extends AppCompatActivity {
+    private GalleryViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        viewModel = new GalleryViewModel();
+
+        ActivityListBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_list);
+        binding.setListModel(viewModel);
 
         SetupList();
+
+        // Fetch Data
+        viewModel.FetchGallery();
     }
 
     private void SetupList() {
@@ -28,5 +41,12 @@ public class ActivityList extends AppCompatActivity {
         final PictureListAdapter adapter = new PictureListAdapter(new ArrayList<SinglePicture>());
         recyclerView.setAdapter(adapter);
 
+        // Data Binding
+        viewModel.getPictureList().observe(this, new Observer<List<SinglePicture>>() {
+            @Override
+            public void onChanged(List<SinglePicture> list) {
+                adapter.updateList(list);
+            }
+        });
     }
 }
