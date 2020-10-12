c0105f353a060c65f435a8842e64b09cb28ba7bc
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:18:37 +0800

    <Add> 初步設定Model, 提供SinglePicture格式與Downloader範本。

---
+++ app/src/main/java/com/cmoney/testlab/model/PictureDownloader.java
@@ -0,0 +1,22 @@
+package com.cmoney.testlab.model;
+
+
+import android.os.Handler;
+
+public class PictureDownloader {
+
+    public void RequestSinglePicture(final int id, final OnFinishCallback callback) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                callback.onSuccess(new SinglePicture(id));
+            }
+        }, 3000);
+    }
+
+    public interface OnFinishCallback {
+        void onSuccess(SinglePicture picture);
+
+        void onFail(String msg);
+    }
+}
---
+++ app/src/main/java/com/cmoney/testlab/model/SinglePicture.java
@@ -0,0 +1,12 @@
+package com.cmoney.testlab.model;
+
+public class SinglePicture {
+    private int id;
+    private String content;
+    private String thumbUrl;
+    private String url;
+
+    public SinglePicture(int id) {
+        this.id = id;
+    }
+}
