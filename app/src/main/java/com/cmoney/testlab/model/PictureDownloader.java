e559ea36325d49db51766610fbd96a35db92b8e2
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 17:23:55 +0800

    <Add> Model: 引入Volley進行初步單一物件查詢設定。

--- app/src/main/java/com/cmoney/testlab/model/PictureDownloader.java
+++ app/src/main/java/com/cmoney/testlab/model/PictureDownloader.java
@@ -1,20 +1,61 @@
 package com.cmoney.testlab.model;
 
 
+import android.content.Context;
 import android.os.Handler;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PictureDownloader {
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/photos";
+
+    private RequestQueue requestQueue;
+
+    @Deprecated
+    public PictureDownloader() {
+    }
+
+    public PictureDownloader(RequestQueue requestQueue) {
+        this.requestQueue = requestQueue;
+    }
+
+    public PictureDownloader(Context context) {
+        this.requestQueue = Volley.newRequestQueue(context);
+    }
 
     public void RequestSinglePicture(final int id, final OnFinishCallback callback) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                callback.onSuccess(new SinglePicture(id));
-            }
-        }, 3000);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                callback.onSuccess(new SinglePicture(id));
+//            }
+//        }, 3000);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(BASE_URL + "/" + id,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callback.onSuccess(new Gson().fromJson(String.valueOf(response), SinglePicture.class));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error != null) callback.onFail(error.getMessage());
+                        else callback.onFail("Null error.");
+                    }
+                });
+        requestQueue.add(objectRequest);
     }
 
     public void RequestPictureGallery(final OnGalleryFinishCallback callback) {
