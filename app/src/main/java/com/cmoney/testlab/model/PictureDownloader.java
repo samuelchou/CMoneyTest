a1dd84d90def11176bd54605652576469c0ef9db
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:33:00 +0800

    <Add> Model: 新增Gallery範本方法，可以獲取Picture清單。

--- app/src/main/java/com/cmoney/testlab/model/PictureDownloader.java
+++ app/src/main/java/com/cmoney/testlab/model/PictureDownloader.java
@@ -3,6 +3,9 @@ package com.cmoney.testlab.model;
 
 import android.os.Handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PictureDownloader {
 
     public void RequestSinglePicture(final int id, final OnFinishCallback callback) {
@@ -14,8 +17,27 @@ public class PictureDownloader {
         }, 3000);
     }
 
+    public void RequestPictureGallery(final OnGalleryFinishCallback callback) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                List<SinglePicture> pictureList = new ArrayList<>();
+                for (int i = 0; i < 100; i++) {
+                    pictureList.add(new SinglePicture(i));
+                }
+                callback.onSuccess(pictureList);
+            }
+        }, 3000);
+    }
+
     public interface OnFinishCallback {
         void onSuccess(SinglePicture result);
+
+        void onFail(String msg);
+    }
+
+    public interface OnGalleryFinishCallback {
+        void onSuccess(List<SinglePicture> result);
 
         void onFail(String msg);
     }
