e2f7c67e05bed86e83b003de2577a49343dad7d4
samuelchou <samuelchou@hotmail.com.tw>
2020-10-12 15:25:18 +0800

    <Fix> SinglePicture: 設定Getter, 為MVVM做準備。

--- app/src/main/java/com/cmoney/testlab/model/SinglePicture.java
+++ app/src/main/java/com/cmoney/testlab/model/SinglePicture.java
@@ -2,11 +2,27 @@ package com.cmoney.testlab.model;
 
 public class SinglePicture {
     private int id;
-    private String content;
+    private String content = "test content";
     private String thumbUrl;
     private String url;
 
     public SinglePicture(int id) {
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
